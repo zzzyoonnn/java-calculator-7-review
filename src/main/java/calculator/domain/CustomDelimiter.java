e1237951292cf4d5ba3c76150f3a02dafa0cec5a
zzzyoonnn <dwsrh03@gmail.com>
2024-11-22 18:12:49 +0900

    refactor: 커스텀 구분자를 추출하는 extractCustomDelimiterTest 수정

--- src/main/java/calculator/domain/CustomDelimiter.java
+++ src/main/java/calculator/domain/CustomDelimiter.java
@@ -1,32 +1,27 @@
 package calculator.domain;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class CustomDelimiter {
     private String customString;
-    private final String startDelimiter = "//";
-    private final String endDelimiter = "\\\n";
 
     public CustomDelimiter(String customString) {
         this.customString = customString;
     }
 
     private String extractCustomDelimiter(String customString) {
-        String regex = startDelimiter + "(.*?)" + endDelimiter;
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(customString);
+        int startIndex = 2;
+        int endIndex = customString.lastIndexOf("\\n");
 
-        return matcher.group(1);
+        return customString.substring(startIndex, endIndex);
     }
 
-    public String getCustomDelimiter(String customString) {
+    public String getCustomDelimiter() {
         return extractCustomDelimiter(customString);
     }
 
 
     private boolean endsWithCustomDelimiter() {
-        return customString.contains("\\n");
+        return customString.contains("\n");
     }
 
     public boolean hasEndsWithCustomDelimiter() {
