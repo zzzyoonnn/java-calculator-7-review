bc23cb7409054c137b03a898a3668a697766c4ac
zzzyoonnn <dwsrh03@gmail.com>
2024-11-22 17:27:17 +0900

    feat: 커스텀 구분자를 추출하는 extractCustomDelimiter 구현

--- src/main/java/calculator/domain/CustomDelimiter.java
+++ src/main/java/calculator/domain/CustomDelimiter.java
@@ -1,11 +1,29 @@
 package calculator.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CustomDelimiter {
     private String customString;
+    private final String startDelimiter = "//";
+    private final String endDelimiter = "\\\n";
 
     public CustomDelimiter(String customString) {
         this.customString = customString;
     }
+
+    private String extractCustomDelimiter(String customString) {
+        String regex = startDelimiter + "(.*?)" + endDelimiter;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(customString);
+
+        return matcher.group(1);
+    }
+
+    public String getCustomDelimiter(String customString) {
+        return extractCustomDelimiter(customString);
+    }
+
 
     private boolean endsWithCustomDelimiter() {
         return customString.contains("\\n");
