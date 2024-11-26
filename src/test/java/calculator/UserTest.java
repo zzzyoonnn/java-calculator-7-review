79fb03db1f393905e45d23f220e0b3c8b40d6677
zzzyoonnn <dwsrh03@gmail.com>
2024-11-26 17:58:51 +0900

    test: 문자열을 분리하는 작업을 시작하는 separateUserInputTest 구현

--- src/test/java/calculator/UserTest.java
+++ src/test/java/calculator/UserTest.java
@@ -1,6 +1,6 @@
 package calculator;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -10,7 +10,6 @@ import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserTest {
@@ -44,7 +43,7 @@ public class UserTest {
     @ParameterizedTest
     @ValueSource(strings = {"//abc\\n1234", "//1a2b3c\\nabc"})
     @DisplayName("커스텀 구분자를 보유하고 있다.")
-    public void testHasCustomDelimiter(String input) throws IOException {
+    public void testHasCustomDelimiter(String input) {
         User user = new User(input);
 
         assertTrue(user.hasCustomDelimiter());
@@ -52,10 +51,21 @@ public class UserTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"", "1,2", "1,2,3", "1,2:3"})
-    @DisplayName("커스텀 구분자를 보유하고 있지 않다..")
-    public void testHasNotCustomDelimiter(String input) throws IOException {
+    @DisplayName("커스텀 구분자를 보유하고 있지 않다.")
+    public void testHasNotCustomDelimiter(String input) {
         User user = new User(input);
 
         assertFalse(user.hasCustomDelimiter());
+    }
+
+
+    @Test
+    @DisplayName("기분 구분자로 문자열을 자를 수 있다.")
+    public void testArrayValues() throws IOException {
+        User user = new User("1,2,3");
+        String[] result = user.processUserInput();
+        String[] expectedArray = {"1", "2", "3"};
+
+        assertArrayEquals(expectedArray, result);
     }
 }
