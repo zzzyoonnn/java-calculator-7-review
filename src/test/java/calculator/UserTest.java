a500568b8d7c5fdeaf7371797c63949b7a5332d4
zzzyoonnn <dwsrh03@gmail.com>
2024-11-28 00:32:10 +0900

    test: 문자열을 분리하는 작업을 시작하는 splitUserInputTest 구현

--- src/test/java/calculator/UserTest.java
+++ src/test/java/calculator/UserTest.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserTest {
@@ -40,31 +41,61 @@ public class UserTest {
         assertTrue(user.isCustomDelimiter(), input);
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"//abc\\n1234", "//1a2b3c\\nabc"})
-    @DisplayName("커스텀 구분자를 보유하고 있다.")
-    public void testHasCustomDelimiter(String input) {
-        User user = new User(input);
+//    @ParameterizedTest
+//    @CsvSource(strings = {"//abc\\n1234, 1234", "//1a2b3c\\nabc, abc"})
+//    @DisplayName("커스텀 구분자를 보유하고 있다.")
+//    public void testHasCustomDelimiter(String input, String expected) {
+//        User user = new User(input);
+//
+//        assertEquals(user.hasCustomDelimiter(), expected);
+//    }
 
-        assertTrue(user.hasCustomDelimiter());
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", "1,2", "1,2,3", "1,2:3"})
-    @DisplayName("커스텀 구분자를 보유하고 있지 않다.")
-    public void testHasNotCustomDelimiter(String input) {
-        User user = new User(input);
-
-        assertFalse(user.hasCustomDelimiter());
-    }
+//    @ParameterizedTest
+//    @ValueSource(strings = {"", "1,2", "1,2,3", "1,2:3"})
+//    @DisplayName("커스텀 구분자를 보유하고 있지 않다.")
+//    public void testHasNotCustomDelimiter(String input) {
+//        User user = new User(input);
+//
+//        assertFalse(user.hasCustomDelimiter());
+//    }
 
 
     @Test
-    @DisplayName("기분 구분자로 문자열을 자를 수 있다.")
-    public void testArrayValues() throws IOException {
+    @DisplayName("기본 구분자로 문자열을 자를 수 있다.")
+    public void testsSplitDefaultDelimiter() {
         User user = new User("1,2,3");
         String[] result = user.processUserInput();
         String[] expectedArray = {"1", "2", "3"};
+
+        assertArrayEquals(expectedArray, result);
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 문자열을 자를 수 있다.")
+    public void testCustomDelimiter() {
+        User user = new User("//;\\n1;2;3");
+        String[] result = user.processUserInput();
+        String[] expectedArray = {"1", "2", "3"};
+
+        assertArrayEquals(expectedArray, result);
+    }
+
+    @Test
+    @DisplayName("기본 구분자와 커스텀 구분자로 문자열을 자를 수 있다.")
+    public void testsSplitDefaultAndCustomDelimiter() {
+        User user = new User("//abc\\n1234,12:34,5");
+        String[] result = user.processUserInput();
+        String[] expectedArray = {"1234", "12", "34", "5"};
+
+        assertArrayEquals(expectedArray, result);
+    }
+
+    @Test
+    @DisplayName("기본 구분자와 커스텀 구분자로 문자열을 자를 수 있다.")
+    public void testsSplitDefaultAndCustomDelimiter2() {
+        User user = new User("//abc\\nab,c:d,5");
+        String[] result = user.processUserInput();
+        String[] expectedArray = {"ab", "c", "d", "5"};
 
         assertArrayEquals(expectedArray, result);
     }
